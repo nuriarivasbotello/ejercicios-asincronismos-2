281b8ed1460917f226df9998a53f82ce30e90928
nuriarivasbotello <nuriarivasbotello@gmail.com>
2023-08-09 01:11:37 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,17 @@ const dailyButton = document.getElementById('daily');
 const weeklyButton = document.getElementById('weekly');
 const monthlyButton = document.getElementById('monthly');
 const mainElement = document.getElementById('main');
-
+const cardElement = document.getElementById('cards');
+let realDates;
 const profile = async () => {
   const respuesta = await fetch('../data/data.json');
   const datos = await respuesta.json();
-  console.log(datos[0]);
+  for (let i = 0; i <= 6; i++) {
+    console.log(datos[i].timeframes.daily);
+  }
 };
 profile();
-const paintedProfile = () => {};
+const paintedProfileDaily = () => {
+  console.log();
+};
+paintedProfileDaily();
