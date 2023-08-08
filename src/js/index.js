09fe4aaa4491959245b9921c6032866de153fc5e
nuriarivasbotello <nuriarivasbotello@gmail.com>
2023-08-08 19:48:46 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+const dailyButton = document.getElementById('daily');
+const weeklyButton = document.getElementById('weekly');
+const monthlyButton = document.getElementById('monthly');
+const mainElement = document.getElementById('main');
+
+const profile = async () => {
+  const respuesta = await fetch('../data/data.json');
+  const datos = await respuesta.json();
+  console.log(datos[0]);
+};
+profile();
+const paintedProfile = () => {};
